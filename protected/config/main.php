f066e104a7ff63325296bdd156e659a598d53e0b
David Aycan <david.aycan@gmail.com>
2013-11-24 23:04:23 -0800

    added twitter bootstrap extension and theme

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,6 +5,12 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+
+// Define a path alias for the Bootstrap extension as it's used internally.
+// In this example we assume that you unzipped the extension under protected/extensions.
+Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
+
+
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Squeez',
@@ -17,6 +23,8 @@ return array(
 		'application.models.*',
 		'application.components.*',
 	),
+	
+	'theme'=>'bootstrap', // requires you to copy the theme under your themes directory
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
@@ -26,12 +34,16 @@ return array(
 			'password'=>'ghettopass',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+			'generatorPaths'=>array(
+                'bootstrap.gii',
+            ),
 		),
 		
 	),
 
 	// application components
 	'components'=>array(
+		
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
@@ -45,7 +57,12 @@ return array(
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
+
 		),
+
+		'bootstrap'=>array(
+            'class'=>'bootstrap.components.Bootstrap',
+        ),
 		
 		/*
 		'db'=>array(
